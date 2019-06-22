0ac70b733de5edc6a1059593c94be4b34dc57c6f
Unknown <ruudvanbakel@yahoo.co.uk>
2019-06-22 17:49:41 +0200

    Redid the forum page

--- HCI_site/Source_Code/javascript/index.js
+++ HCI_site/Source_Code/javascript/index.js
@@ -1,6 +1,10 @@
 // JavaScript source code
 $(document).ready(function () {
     $(".factstext").hide();
+
+    $("body").on('submit', function (e) {
+        e.preventDefault();
+    });
 });
 
 function toggleHideElement(element) {
@@ -8,5 +12,32 @@ function toggleHideElement(element) {
         $(element).show();
     } else {
         $(element).hide();
+    }
+}
+
+function postComment() {
+    var comment = $("#comment").val();
+    while (comment.includes("\r\n")) {
+        comment = comment.replace("\r\n", "<br />");
+    }
+    while (comment.includes("\r")) {
+        comment = comment.replace("\r", "<br />");
+    }
+    while (comment.includes("\n")) {
+        comment = comment.replace("\n", "<br />");
+    }
+
+    if (comment.length == 0) {
+        alert("You can't post an empty message.");
+    }
+    else if (comment.length > 500) {
+        alert("Your message may not exceed 500 characters.");
+    }
+    else {
+        var $button = $('#comment-template').clone();
+        $button.children().last().children().last().html(comment);
+        $("#posts").append($button);
+        $("#posts").children().last().removeAttr("hidden");
+        $("#comment").val("");
     }
 }
